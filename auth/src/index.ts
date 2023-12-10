db2fa892e162e7c2e28178215ad1032e606468ea
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-10 21:30:31 +0100

    auth router

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {currentUserRouter} from './routes/current-user';
+import {signupRouter} from './routes/signup';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
 
-const app = express();
-app.use(json());
 const port = 3000;
+const app = express();
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use(json());
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
