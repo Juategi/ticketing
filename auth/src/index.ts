ccc7ad2b1adf3d479b77a938ca90aa1cc1090457
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-10 18:42:15 +0100

    Initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import {json} from 'body-parser';
+
+const app = express();
+app.use(json());
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
