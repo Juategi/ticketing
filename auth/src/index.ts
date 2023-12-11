10c0bacf79020a6d7ccab47d815426f328c897d8
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-11 17:20:11 +0100

    error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
+import {errorHandler} from './middlewares/error-handler';
 
 const port = 3000;
 const app = express();
@@ -13,6 +14,7 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
