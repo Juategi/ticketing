7dff9046460a6b00e1a3b22ac81d46766ae4ffe9
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-12 13:53:08 +0100

    not found

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
 import {errorHandler} from './middlewares/error-handler';
+import {NotFoundError} from './errors/not-found-error';
 
 const port = 3000;
 const app = express();
@@ -14,6 +16,11 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(port, () => {
