e0d12aa5ae4c8f7d0801884cd1daadc37df14c36
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-12 21:07:09 +0100

    password hash

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../utils/password";
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -23,6 +24,14 @@ interface UserDoc extends mongoose.Document {
 const userSchema: mongoose.Schema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (done) {
+    if (this.isModified("password")) {
+        const hashed = await Password.toHash(this.get("password"));
+        this.set("password", hashed);
+    }
+    done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
