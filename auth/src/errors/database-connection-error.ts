ed980611f843e1629ade7efb701f73c7287b9a90
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-12 13:03:48 +0100

    error handler

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,18 @@
 
-export class DatabaseConnectionError extends Error{
+export class DatabaseConnectionError extends Error implements CustomError{
+
     reason = 'Error connecting to database';   
+    statusCode  = 500;
+
     constructor(){
         super();
         // Only because we are extending a built in class
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors(){
+        return [
+            { message: this.reason }
+        ];         
     }
 }
