96323a2b3348358bd59a733eba355aec35bb6677
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-12-13 13:32:39 +0100

    map response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -24,6 +24,15 @@ interface UserDoc extends mongoose.Document {
 const userSchema: mongoose.Schema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
 });
 
 // This is a mongoose middleware function that will run
